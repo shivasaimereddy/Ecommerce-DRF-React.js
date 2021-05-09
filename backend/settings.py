4d544c8e4c48059336de95941a041ba248365fc7
shivasaimereddy <shivasaimereddy@gmail.com>
2021-05-09 11:31:54 +0530

    Updated

--- backend/settings.py
+++ backend/settings.py
@@ -181,10 +181,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-    BASE_DIR / 'frontend/build/static'
-]
 
 MEDIA_ROOT = 'static/images'
 
