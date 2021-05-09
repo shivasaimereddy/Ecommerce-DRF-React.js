da3466815c6c8b2a589e145f8470325abb5229c1
shivasaimereddy <shivasaimereddy@gmail.com>
2021-05-09 18:51:35 +0530

    Added image_url field to models

--- base/models.py
+++ base/models.py
@@ -68,6 +68,7 @@ class OrderItem(models.Model):
     price = models.DecimalField(
         max_digits=7, decimal_places=1, null=True, blank=True)
     image = models.CharField(max_length=200, null=True, blank=True)
+    image_url = models.CharField(max_length=200, null=True, blank=True)
     _id = models.AutoField(primary_key=True, editable=False)
 
     def __str__(self):
